79e122dad49d8f14956a0922da1370ceef95119f
izayacity <xyang42@ucsc.edu>
2017-04-10 00:23:18 -0700

    Increase/decrease particle number by UP/DOWN. Feature 1 finished.

--- include/ParticleSystem.h
+++ include/ParticleSystem.h
@@ -8,9 +8,13 @@ private:
 		sf::Time lifetime;
 	};
 
-	// Particle STD vector
+	// default initial count of particles
+	const int COUNT = 1000;
+	// count of particles
+	unsigned int count;
+	// Particle STD vector storing the velocity and life time
 	std::vector<Particle> m_particles;
-	// SFML Vertex Array
+	// SFML Vertex Array storing point shape
 	sf::VertexArray m_vertices;
 	// Life time of particles
 	sf::Time m_lifetime;
@@ -42,11 +46,12 @@ private:
 	}
 
 public:
-	ParticleSystem (unsigned int count) :
-		m_particles (count),
-		m_vertices (sf::Points, count),
+	ParticleSystem () :
+		m_particles (COUNT),
+		m_vertices (sf::Points, COUNT),
 		m_lifetime (sf::seconds (3)),
 		m_emitter (0, 0) {
+		count = 1000;
 	}
 
 	// Set the location of the emitter
@@ -72,5 +77,56 @@ public:
 			float ratio = p.lifetime.asSeconds () / m_lifetime.asSeconds ();
 			m_vertices[i].color.a = static_cast<sf::Uint8>(ratio * 255);
 		}
+	}
+
+	// get the count of particles
+	int getCount () {
+		return count;
+	}
+
+	// add the count of particles by 100 each time
+	void addCount () {
+		int diff = 100;
+
+		if (count >= UINT_MAX - 1) {
+			diff = 0;
+		} else if (count + 100 >= UINT_MAX) {
+			diff = UINT_MAX - 1 - count;
+		}
+
+		// update particles and vertice
+		m_vertices.resize (count + diff);
+		m_particles.resize (count + diff);
+		count = m_vertices.getVertexCount ();
+
+		/*while (diff > 0) {
+			Particle* particle = new Particle;
+			sf::Vertex* point = new sf::Vertex;
+
+			if (particle != nullptr && point != nullptr) {
+				m_particles.push_back (*particle);
+				m_vertices.append (*point);
+				count++;
+				diff--;
+			} else {
+				throw std::runtime_error ("Failed to add particle");
+			}
+		}*/
+	}
+
+	// reduce the count of particles by 100 each time
+	void reduceCount () {
+		int diff = 100;
+
+		if (count <= 0) {
+			diff = 0;
+		} else if (count - 100 <= 0) {
+			diff = count;
+		}
+
+		// update particles and vertice
+		m_vertices.resize (count - diff);
+		m_particles.resize (count - diff);
+		count = m_vertices.getVertexCount ();
 	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,93 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <cassert>
 #include "../include/ParticleSystem.h"
 
 int main () {
+	const int gameWidth = 800;
+	const int gameHeight = 600;
 	// create the window
-	sf::RenderWindow window (sf::VideoMode (800, 600), "Particles");
+	sf::RenderWindow window1 (sf::VideoMode (gameWidth, gameHeight), "ParticleSys1");
+	//sf::RenderWindow window2 (sf::VideoMode (gameWidth, gameHeight), "ParticleSys2");
 
 	// create the particle system
-	ParticleSystem particles (1000);
+	ParticleSystem particles1;
+	ParticleSystem particles2;
 
 	// create a clock to track the elapsed time
 	sf::Clock clock;
+	sf::Clock clock2;
 
+	sf::RectangleShape textField;
+	textField.setFillColor (sf::Color::Black);
+	textField.setSize (sf::Vector2f (800.f, 30.f));
+	textField.setPosition (0, gameHeight - 30);
+
+	// load fonts
+	sf::Font fontHNL;	
+	assert (fontHNL.loadFromFile ("static/font/HelveticaNeue Light.ttf"));
+
+	sf::Text countText;
+	countText.setFont (fontHNL);
+	countText.setCharacterSize (20);
+	countText.setFillColor (sf::Color::Green);
+	std::string countStr = std::to_string (particles1.getCount ());
+	countText.setString ("Particle count (Use UP/DOWN key to change): " + countStr);
+	countText.setPosition (20, gameHeight - 30);
+	
 	// run the main loop
-	while (window.isOpen ()) {
+	while (window1.isOpen ()) {
 		// handle events
 		sf::Event event;
-		while (window.pollEvent (event)) {
-			if (event.type == sf::Event::Closed)
-				window.close ();
+		while (window1.pollEvent (event)) {
+			if ((event.type == sf::Event::Closed) ||
+				((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))) {
+				window1.close ();
+			} else if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Up)) {
+				particles1.addCount ();
+				countStr = std::to_string (particles1.getCount ());
+				countText.setString ("Particle count (Use UP/DOWN key to change): " + countStr);
+			} else if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Down)) {
+				particles1.reduceCount ();
+				countStr = std::to_string (particles1.getCount ());
+				countText.setString ("Particle count (Use UP/DOWN key to change): " + countStr);
+			}
 		}
 
-		// make the particle system emitter follow the mouse
-		sf::Vector2i mouse = sf::Mouse::getPosition (window);
-		particles.setEmitter (window.mapPixelToCoords (mouse));
+		// * make the particle system emitter follow the mouse
+		sf::Vector2i mouse = sf::Mouse::getPosition (window1);
+		particles1.setEmitter (window1.mapPixelToCoords (mouse));
 
-		// update it
+		// update the particle system by time
 		sf::Time elapsed = clock.restart ();
-		particles.update (elapsed);
+		particles1.update (elapsed);
 
-		// draw it
-		window.clear ();
-		window.draw (particles);
-		window.display ();
+		// render it
+		window1.clear ();
+		window1.draw (particles1);
+		window1.draw (textField);
+		window1.draw (countText);
+		window1.display ();
+
+		/*
+		Window2
+		*/
+		// handle events
+		//sf::Event event2;
+		//while (window2.pollEvent (event2)) {
+		//	switch (event.type) {
+		//	case sf::Event::Closed:
+		//		window1.close ();
+		//		break;
+		//	//case sf::Event::KeyPressed:
+
+		//	}
+		//}
+
+		//// render it
+		//window2.clear ();
+		////window2.draw (terminal);
+		//window2.display ();
 	}
 
 	return 0;
